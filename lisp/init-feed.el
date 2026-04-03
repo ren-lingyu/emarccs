f0f0c3ca6ee3cd4fcccf345119b2d7df1fd78ff2
ren-lingyu <Ren_Lingyu@outlook.com>
2026-04-03 09:34:50 +0000

    Update `lisp/init-feed.el`

--- lisp/init-feed.el
+++ lisp/init-feed.el
@@ -2,8 +2,26 @@
 ;;; commentary:
 ;;; code:
 
+(use-package elfeed
+  :straight (:host github :repo "skeeto/elfeed")
+  :config
+  (setq elfeed-feeds '(("https://langxubai.com/rss.xml" blog))))
 
-(use-package arxiv-mode)
+(use-package elfeed-dashboard
+  :disabled
+  :straight (:host github :repo "manojm321/elfeed-dashboard"))
+
+(use-package elfeed-org
+  :disabled
+  :straight (:host github :repo "remyhonig/elfeed-org"))
+
+(use-package arxiv-mode
+  :straight (:host github :repo "fizban007/arxiv-mode")
+  :config
+  (setq arxiv-use-variable-pitch t)
+  (setq arxiv-pop-up-new-frame t)
+  (setq arxiv-default-category "cond-mat")
+  (setq arxiv-frame-alist '((name . "*arXiv*"))))
 
 (provide 'init-feed)
 ;;; init-feed.el ends here.

aa7961c817a325f4156447b0cf40d17bf4cbc66a
ren-lingyu <Ren_Lingyu@outlook.com>
2026-07-17 18:31:09 +0800

    fix(org): use exact Emacs binary path in preBuild hook
    - 将 `preBuild` 钩子中的 `emacs` 替换为 `pkgs.lib.getExe' pkgs.emacs "emacs"`.

--- pkgs/org/override.nix
+++ pkgs/org/override.nix
@@ -3,7 +3,7 @@
   input = final_ : super_ : {
     preBuild = builtins.concatStringsSep ("\n") [
       "export EMARCCS_ORG_GIT_VERSION=\"${builtins.substring 0 8 (super_.src.rev or "unknown")}\""
-      "emacs --batch -Q --load ${./pre-build.el}"
+      "${pkgs.lib.getExe' pkgs.emacs "emacs"} --batch -Q --load ${./pre-build.el}"
     ];
   };
 

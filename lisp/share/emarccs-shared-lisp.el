57a5b24cd2d9e34a4a6878f25aafe5e1a65e49a1
ren-lingyu <Ren_Lingyu@outlook.com>
2026-09-25 22:11:11 +0800

    feat(sly): configure sly with sbcl
    - 在 `emarccs-shared-lisp.el` 中新增 `use-package sly`, 使用 `sbcl` 作为 `inferior-lisp-program`.

--- lisp/share/emarccs-shared-lisp.el
+++ lisp/share/emarccs-shared-lisp.el
@@ -2,6 +2,11 @@
 ;;; commentary:
 ;;; code:
 
+(use-package sly
+  :commands sly
+  :custom
+  (inferior-lisp-program "sbcl"))
+
 (provide 'emarccs-shared-lisp)
 
 ;;; emarccs-shared-lisp.el ends here
